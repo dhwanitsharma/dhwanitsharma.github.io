097721f74654d7dc56dbca502c3c2b3fa368ca09
dhwanitsharma <dhwanitsharma5@gmail.com>
2020-12-01 05:30:12 +0530

    new deployment

---
+++ precache-manifest.e3e6bb10304e38285cc285712d7c2ebb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0813e30a9d4e505eef7f605836e3ddb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e214bb3519a64a2d9d53",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cb70a67cffec224e160b",
+    "url": "/static/js/2.de8053c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.de8053c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e214bb3519a64a2d9d53",
+    "url": "/static/js/main.b70f19f5.chunk.js"
+  },
+  {
+    "revision": "824a87c525fc41ffa25d",
+    "url": "/static/js/runtime-main.078ffda6.js"
+  }
+]);
